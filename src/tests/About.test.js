5e6de064caafe2f2788bbfd61e21530da305afc7
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-10-20 19:36:48 -0300

    feat: test if has image

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { getAllByText, render } from '@testing-library/react';
 import About from '../components/About';
 
 describe('tests About component', () => {
@@ -22,5 +22,12 @@ describe('tests About component', () => {
     const paragraphs = container.querySelectorAll('p');
     const two = 2;
     expect(paragraphs.length).toBe(two);
+  });
+
+  it('has a image of a `Pokédex`', () => {
+    const { getByAltText } = render(<About />);
+    const image = getByAltText(/Pokédex/i);
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
