1be012ba0d45c159779456c1c5c6c63ee0cba210
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-10-20 17:43:52 -0300

    feat: test the link navigations

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,24 @@
+import { getByText } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
-describe('Test for App component', () => {
+describe('Tests for App component', () => {
   it('renders the home page with the path `/`', () => {
-    const { history } = renderWithRouter(<App />);
+    const { getByText, history } = renderWithRouter(<App />);
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
     const { pathname } = history.location;
     expect(pathname).toBe('/');
-  })
+  });
+
+  it('renders links navigation on top', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText(/Home/i);
+    expect(home).toBeInTheDocument();
+    const about = getByText(/About/i);
+    expect(about).toBeInTheDocument();
+    const favorites = getByText(/Favorite Pokémons/i);
+    expect(favorites).toBeInTheDocument();
+  });
 });
