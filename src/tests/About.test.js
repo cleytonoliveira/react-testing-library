5c39e487462d7f9b136b1414d2161fcfbe756675
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-10-20 18:27:45 -0300

    feat: test information about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('tests About component', () => {
+  it('page has a information about the pokédex', () => {
+    const { getByText } = render(<About />);
+    const about = getByText(/This application simulates a Pokédex/i);
+    expect(about).toBeInTheDocument();
+  });
+});
