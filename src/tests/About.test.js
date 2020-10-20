edab35dab2ac35c4a107d36ffab01333a64d0288
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-10-20 18:46:33 -0300

    feat: test with has a heading

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -8,4 +8,12 @@ describe('tests About component', () => {
     const about = getByText(/This application simulates a Pokédex/i);
     expect(about).toBeInTheDocument();
   });
+
+  it('page has a heading h2 with text `About Pokédex`', () => {
+    const { getByText, getByRole } = render(<About />);
+    const h2 = getByRole('heading', { level: 2 });
+    expect(h2).toBeInTheDocument();
+    const heading = getByText(/About Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
 });
