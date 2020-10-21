faa7748e0361a5f605aba2a3583a38619614860e
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-10-21 19:12:37 -0300

    feat: test the next pokemon when click on button

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+const pokemonList = [
+  'Cartepie', 'Ekans', 'Alakazam', 'Mew', 'Rapidash', 'Snorlax', 'Dragonair'];
+
+describe('test Pokedex component', () => {
+  it('render a next pokemon when click in `Próximo pokémon`', () => {
+    const { getByText, getByRole } = renderWithRouter(<App />);
+    const button = getByRole('button', { name: 'Próximo pokémon' });
+    expect(button).toBeInTheDocument();
+    const pikachu = getByText(/Pikachu/i);
+    expect(pikachu).toBeInTheDocument();
+    fireEvent.click(button);
+    const charmander = getByText(/Charmander/i);
+    expect(charmander).toBeInTheDocument();
+    pokemonList.forEach(() => fireEvent.click(button));
+    const dragonair = getByText(/Dragonair/i);
+    expect(dragonair).toBeInTheDocument();
+    fireEvent.click(button);
+    expect(pikachu).toBeInTheDocument();
+  });
+});
