d1c874fe2f71819696fb7d0f317c2f9a27be99ea
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-10-21 16:12:19 -0300

    feat: test render heading with text not found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('tests of NotFound component', () => {
+  it('render a heading `h2` with de text `Page requested not found`', () => {
+    const { getByRole, getByText } = render(<NotFound />);
+    const h2 = getByRole('heading', { level: 2 });
+    expect(h2).toBeInTheDocument();
+    const text = getByText(/Page requested not found/i);
+    expect(text).toBeInTheDocument();
+  });
+});
