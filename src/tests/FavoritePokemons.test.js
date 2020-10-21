8f774b7b3fe5631f66bd658caf0ae9f190179845
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-10-21 12:41:24 -0300

    feat: test a message without favorite pokemon

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('tests for Favorite Pokemons component', () => {
+  it('render a message `No favorite pokemon found` without a favorite pokemons', () => {
+    const { getByText } = render(<FavoritePokemons />);
+    const favorite = getByText(/No favorite pokemon found/i);
+    expect(favorite).toBeInTheDocument();
+  });
+});
