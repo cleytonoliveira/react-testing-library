c729469f3fdf3e456569ff2c280ab48c5fd8989a
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-10-27 13:50:16 -0300

    feat: test redirect to details page

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { fireEvent, getByRole } from '@testing-library/react';
 import { Pokemon } from '../components';
 import pokemons from '../data';
 import renderWithRouter from '../renderWithRouter';
@@ -10,10 +11,33 @@ describe('test Pokemon Component', () => {
     />);
     const name = getByTestId('pokemon-name');
     expect(name).toBeInTheDocument();
+    const type = getByTestId('pokemonType');
+    expect(type).toHaveTextContent(/Electric/i);
+    expect(name).toHaveTextContent(/Pikachu/i);
     const weight = getByTestId('pokemon-weight');
     expect(weight).toBeInTheDocument();
     expect(weight).toHaveTextContent(/Average weight: 6.0 kg/i);
     const image = getByAltText(/Pikachu sprite/i);
     expect(image).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+
+  it('test link navigation to render details about pokémon', () => {
+    const { getByRole } = renderWithRouter(<Pokemon
+      pokemon={ pokemons[0] }    
+    />);
+    const linkDetails = getByRole('link', { name: /More details/i });
+    expect(linkDetails).toBeInTheDocument();
+    expect(linkDetails).toHaveAttribute('href', '/pokemons/25');
+  });
+
+  it('redirect to details page', () => {
+    const { getByRole, history } = renderWithRouter(<Pokemon
+      pokemon={ pokemons[0] }
+      />);
+    const linkDetails = getByRole('link', { name: /More details/i });
+    expect(linkDetails).toBeInTheDocument();
+    fireEvent.click(linkDetails);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
   });
 });
