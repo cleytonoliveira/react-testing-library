5a3c8002fe4b778f68d1ea2b166ded8325299887
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-10-27 12:40:06 -0300

    feat: test render a card with the pokemon information

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { Pokemon } from '../components';
+import pokemons from '../data';
+import renderWithRouter from '../renderWithRouter';
+
+describe('test Pokemon Component', () => {
+  it('render a card with the pokemon informations', () => {
+    const { getByTestId, getByAltText } = renderWithRouter(<Pokemon
+      pokemon={ pokemons[0] }
+    />);
+    const name = getByTestId('pokemon-name');
+    expect(name).toBeInTheDocument();
+    const weight = getByTestId('pokemon-weight');
+    expect(weight).toBeInTheDocument();
+    expect(weight).toHaveTextContent(/Average weight: 6.0 kg/i);
+    const image = getByAltText(/Pikachu sprite/i);
+    expect(image).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+});
